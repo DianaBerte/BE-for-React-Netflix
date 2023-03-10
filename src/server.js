1f2bd834bfc6329f0c40e8845a640d24153ce1db
DianaBerte <dianaberte.go@gmail.com>
2023-03-10 12:02:18 +0100

    Implements server.js

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import Express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import { join } from "path";
+import createHttpError from "http-errors";
+
+const server = Express()
+const port = process.env.PORT || 3001
+const publicFolderPath = join(process.cwd(), "./public")
+
+// ********************************************* CORS ************************************************
+
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
+
+server.use(Express.static(publicFolderPath))
+server.use(
+    cors({
+        origin: (currentOrigin, corsNext) => {
+            if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+                corsNext(null, true)
+            } else {
+                corsNext(createHttpError(400, `Origin ${currentOrigin} is not in the whitelist!`))
+            }
+        },
+    })
+)
+server.use(Express.json())
+
+// ************************** ENDPOINTS ***********************
+
+server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log(`Server is running on port ${port}`)
+})
